a57e9443271246e397dd3eacef991ed1187d913a
mission biswas <missionbiswas9@gmail.com>
2022-09-26 11:59:15 +0600

    mission

---
+++ src/component/Shop/Shop.js
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import Product from '../Product/Product';
+import './Shop.css'
+
+const Shop = () => {
+
+    const [products, setProduct] = useState([]);
+    const [cart, setCart] = useState([]);
+
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProduct(data))
+    }, []);
+
+    const handleAddToCart = (product) => {
+        console.log(product)
+        const newCart = [...cart, product]
+        setCart(newCart)
+    }
+
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                {
+                    products.map(product => <Product
+                        key={product.id}
+                        product={product}
+                        handleAddToCart={handleAddToCart}
+                    ></Product>)
+                }
+            </div>
+
+            <div className="cart-container">
+                <h1>Order Summry</h1>
+                <p>Selected Item: {cart.length}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
